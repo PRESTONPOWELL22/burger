e8e424a1e08b18060e4ef542fe18fe799def6827
Preston Powell <prestonpowell@Prestons-MacBook-Pro.local>
2018-04-25 16:24:20 -0700

    got a lot further

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var express = require('express')
+
 var app = express()
+var PORT = 3000
+
+var exphbs = require('express-handlebars')
 var bodyParser = require('body-parser')
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log('app listening on port: ' + PORT)
+})
 
